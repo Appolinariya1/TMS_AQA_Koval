540caf7a02c9cdb5bcd84e0fa12f18dc8d472502
Appolinariya Koval <polinaholod97@gmail.com>
2023-11-18 21:29:08 +0300

    SecondTask created

---
+++ TypesAndOperators/SecondTask.cs
@@ -0,0 +1,63 @@
+namespace TypesAndOperators;
+
+public static class SecondTask
+{
+    public static void CheckNumberInIntervals()
+    {
+        //Задание 2. Проверка попадания числа в интервалы
+
+        Console.Write("Введите число: ");
+        int a = GetNumberFromConsole();
+
+        if (a > 50 || a < -50)
+        {
+            Console.WriteLine("Некорректное число");
+            return;
+        }
+
+        if (a >= -40 && a <= -10)
+        {
+            Console.WriteLine("Число попадает в промежуток [-40, -10]");
+            return;
+        }
+
+        if (a >= -9 && a <= 0)
+        {
+            Console.WriteLine("Число попадает в промежуток [-9, 0]");
+            return;
+        }
+
+        if (a >= 1 && a <= 10)
+        {
+            Console.WriteLine("Число попадает в промежуток [1, 10]");
+            return;
+        }
+
+        if (a >= 11 && a <= 40)
+        {
+            Console.WriteLine("Число попадает в промежуток [11, 40]");
+            return;
+        }
+
+        Console.WriteLine("Число не попадает ни в один из заданных интервалов");
+    }
+
+    private static int GetNumberFromConsole()
+    {
+        string? inputString = Console.ReadLine();
+        if (string.IsNullOrEmpty(inputString))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            return GetNumberFromConsole();
+        }
+
+        int result;
+        if (!int.TryParse(inputString, out result))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            return GetNumberFromConsole();
+        }
+
+        return result;
+    }
+}
