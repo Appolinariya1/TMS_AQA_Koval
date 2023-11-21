13150181b84943bbda098ec598c94106b5a6f609
Appolinariya Koval <polinaholod97@gmail.com>
2023-11-21 19:22:01 +0300

    Initial for StringApp

---
+++ StringApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace StringApp
+{
+    static class Programm
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
