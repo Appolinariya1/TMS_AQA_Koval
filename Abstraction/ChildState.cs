64634fd401391a8c8e90507a758b560790771edd
Appolinariya Koval <polinaholod97@gmail.com>
2023-12-02 13:06:31 +0300

    14.1 Lesson

---
+++ Abstraction/ChildState.cs
@@ -0,0 +1,11 @@
+using Encapsulation;
+namespace Abstraction;
+
+public class ChildState : State
+{
+    void Test()
+    {
+        base.PrintProtectedInternal();
+        // base.PrintProtectedPrivate(); - Ошибка
+    }
+}
--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data;
 using Abstraction;
 using Abstraction;
+using Encapsulation;
 
 Client client1 = new Client();
 client1.Id = 1;
@@ -20,3 +23,15 @@ Console.Out.WriteLine(client1.PhoneNumber);
 
 Console.Out.WriteLine(client2.Id);
 Console.Out.WriteLine(client2.PhoneNumber);
+
+State state = new State();
+// переменная publicVar общедоступна
+
+Console.WriteLine(state.publicVar);
+state.PrintPublic();
+
+
+// state.internalVar = ""; - Ошибка
+// state.PrintInternal(); - Ошибка
+// state.PrintProtectedInternal(); - Ошибка
+// state.PrintProtectedPrivate();  - Ошибка
