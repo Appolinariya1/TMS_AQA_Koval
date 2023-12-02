fde2994f889362d58c771f983b0255b47a4486be
Appolinariya Koval <polinaholod97@gmail.com>
2023-12-02 13:39:59 +0300

    Updated 14 Lesson Poly

---
+++ Polymorphism/ChildIndividual.cs
@@ -0,0 +1,8 @@
+namespace Polymorphism;
+
+public class ChildIndividual : Individual
+{
+    public ChildIndividual(int id) : base(id)
+    {
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿using Polymorphism;
+
+Client client = new Client(1);
+client.CountBill(10, 10);
+client.CountBill(10, 10, 10);
+
+// See https://aka.ms/new-console-template for more information
+
+Individual individual1 = new Individual(1);
+individual1.SendNotification();
+// individual1.Address = "Individual Address";
+//
+//
+Corporate corporate1 = new Corporate(1);
+corporate1.SendNotification();
+// corporate1.Address = "Office Address";
+//
+//
+// Console.WriteLine(individual1.Address);
+// Console.WriteLine(corporate1.Address);
+
+ChildIndividual childIndividual = new ChildIndividual(10);
+childIndividual.SendNotification();
+
+ChildCorporate childCorporate = new ChildCorporate(11);
+childCorporate.SendNotification();
