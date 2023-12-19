9d4df7451b3c6ef268c785eb781d7f3a76d0df66
Appolinariya Koval <polinaholod97@gmail.com>
2023-12-19 23:52:54 +0300

    21 Lesson. Generics

---
+++ Generics/VaryGenericPerson.cs
@@ -0,0 +1,9 @@
+namespace Generics;
+
+public class VaryGenericPerson<T> : GenericPerson<int>
+{
+    public T code;
+    public VaryGenericPerson(int id, string name) : base(id, name)
+    {
+    }
+}
