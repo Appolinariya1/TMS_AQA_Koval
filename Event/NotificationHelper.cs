10c5b7783db642602e092472aa5b27e9b3f7de49
Appolinariya Koval <polinaholod97@gmail.com>
2024-01-16 17:56:13 +0300

    25 Lesson LINQ

--- Event/NotificationHelper.cs
+++
@@ -1,9 +0,0 @@
-namespace Event;
-
-public class NotificationHelper
-{
-    public void NotifyByEmail(string email)
-    {
-        Console.WriteLine($"Notfication was send with message {email}");
-    }
-}
