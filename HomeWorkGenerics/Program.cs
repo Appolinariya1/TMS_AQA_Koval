6df706c51f224f72c2028eb0a1e630a17e599e16
Appolinariya Koval <polinaholod97@gmail.com>
2024-01-20 20:52:46 +0300

    SecondTask created

--- HomeWorkGenerics/Program.cs
+++ HomeWorkGenerics/Program.cs
@@ -1,11 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using HomeWorkGenerics.FirstTask;
+using HomeWorkGenerics.SecondTask;
 
 Console.WriteLine("Задание 1");
 
-var pointInt = new Point<int>(2,4);
+var pointInt = new Point<int>(2, 4);
 pointInt.Print();
 
-var pointDouble = new Point<double>(2.5,4.5);
+var pointDouble = new Point<double>(2.5, 4.5);
 pointDouble.Print();
+
+Console.WriteLine("\nЗадание 2");
+Console.WriteLine("Массив с типом int");
+{
+    var intArray = new GenericArray<int>(new int[] { -5, 0, 1 });
+    intArray.Add(3);
+    intArray.Add(5);
+    intArray.Print();
+
+    intArray.RemoveAt(1);
+    intArray.Print();
+
+    int element = intArray.At(3);
+    Console.WriteLine($"Элемент по заданному индексу = {element}");
+
+    int arraylength = intArray.GetLength();
+    Console.WriteLine($"Длина массива = {arraylength}");
+}
+
+Console.WriteLine("\nМассив с типом string");
+{
+    var strArray = new GenericArray<string>(new string[] { "Кошка", "Мышка" });
+    strArray.Add("Кот");
+    strArray.Add("Котенок");
+    strArray.Print();
+
+    strArray.RemoveAt(1);
+    strArray.Print();
+
+    string element1 = strArray.At(0);
+    Console.WriteLine($"Элемент по заданному индексу = {element1}");
+
+    int arraylength1 = strArray.GetLength();
+    Console.WriteLine($"Длина массива = {arraylength1}");
+}
---
+++ HomeWorkGenerics/SecondTask/GenericArray.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+
+namespace HomeWorkGenerics.SecondTask;
+
+public class GenericArray<T>
+{
+    private T[] _array;
+
+    public GenericArray(T[] array)
+    {
+        _array = array;
+    }
+
+    public GenericArray()
+    {
+        _array = new T[] { };
+    }
+
+    public void Add(T element)
+    {
+        Array.Resize(ref _array, GetLength() + 1);
+        _array[GetLength() - 1] = element;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= GetLength())
+        {
+            throw new IndexOutOfRangeException(message:"Некорректный индекс массива");
+        }
+
+        T[] result = new T[] { };
+        Array.Resize(ref result, GetLength() - 1);
+        if (index > 0)
+        {
+            Array.Copy(_array, 0, result, 0, index);
+        }
+
+        if (index < (GetLength() - 1))
+        {
+            Array.Copy(_array, index + 1, result, index, GetLength() - index - 1);
+        }
+
+        _array = result;
+    }
+
+    public T At(int index)
+    {
+        if (index < 0 || index >= GetLength())
+        {
+            throw new IndexOutOfRangeException(message:"Некорректный индекс массива");
+        }
+
+        return _array[index];
+    }
+
+    public int GetLength()
+    {
+        return _array.Length;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(String.Join(", ", _array));
+    }
+}
