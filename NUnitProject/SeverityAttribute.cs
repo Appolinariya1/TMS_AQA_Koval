2adf739bf97cce3a164ab8603f828b782e2f346d
Appolinariya Koval <polinaholod97@gmail.com>
2024-01-23 20:05:39 +0300

    29L. Attributes (random, range, values, valuesource). Hard, soft asserts

---
+++ NUnitProject/SeverityAttribute.cs
@@ -0,0 +1,6 @@
+namespace NUnitProject;
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class SeverityAttribute : PropertyAttribute
+{
+    public SeverityAttribute(SeverityLevel level) : base(level.ToString()) {}
+}
