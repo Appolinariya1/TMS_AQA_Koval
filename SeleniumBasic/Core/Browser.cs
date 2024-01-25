0aa7ee4df3a938aef9f8d1668e352baaedf1709b
Appolinariya Koval <polinaholod97@gmail.com>
2024-01-25 20:24:24 +0300

    30L. Selenium WebDriver
    Способы поднять браузер

---
+++ SeleniumBasic/Core/Browser.cs
@@ -0,0 +1,24 @@
+using NUnitTest.Utilites.Configuration;
+using OpenQA.Selenium;
+//Лучший вариант (все сделается само - скачается веб драйвер в зависимости от версии хрома, пропишется путь и т.д.)
+namespace NUnitTest.Core
+{
+    public class Browser
+    {
+        public IWebDriver? Driver { get; }
+
+        public Browser()
+        {
+            Driver = Configurator.BrowserType?.ToLower() switch
+            {
+                "chrome" => new DriverFactory().GetChromeDriver(),
+                "firefox" => new DriverFactory().GetFirefoxDriver(),
+                _ => Driver
+            };
+
+            Driver?.Manage().Window.Maximize();
+            Driver?.Manage().Cookies.DeleteAllCookies();
+            //Driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+        }
+    }
+}
