502396df2138b25801321c4178176f4383254b1b
Appolinariya Koval <polinaholod97@gmail.com>
2024-02-10 11:40:50 +0300

    35L. PageObject. Steps

--- PageObjectSimple/Tests/LoginTest.cs
+++ PageObjectSimple/Tests/LoginTest.cs
@@ -1,3 +1,4 @@
+using NUnitTest.Pages;
 using SeleniumBasic.Helpers.Configuration;
 using SeleniumBasic.Pages;
 
@@ -8,7 +9,39 @@ public class LoginTest : BaseTest
     [Test]
     public void SuccessfulLoginTest()
     {
-        var loginPage = new LoginPage(Driver);
-        loginPage.EmailInput.SendKeys(Configurator.AppSettings.Username);
+        // Actions = Действия
+        Assert.That(new LoginPage(Driver)
+            .SuccessFulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password)
+            .IsPageOpened());
+        
+        // Проверка
+        Assert.That(
+            UserSteps.SuccessfulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password)
+                .TitleLabel.Text.Trim(), 
+            Is.EqualTo("All Projects"));
+    }
+    
+    [Test]
+    public void InvalidUsernameLoginTest()
+    {
+        // Проверка
+        Assert.That(
+            new LoginPage(Driver)
+                .IncorrectLogin("ssdd", "")
+                .ErrorLabel.Text.Trim(), 
+            Is.EqualTo("Email/Login or Password is incorrect. Please try again."));
+    }
+    
+    [Test]
+    public void SuccessfulLoginStepsTest()
+    {
+        // Actions = Действия
+        
+        
+        // Проверка
+        Assert.That(
+            UserSteps.SuccessfulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password)
+                .TitleLabel.Text.Trim(), 
+            Is.EqualTo("All Projects"));
     }
 }
