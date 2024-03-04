92a08943b09ce204431ce1733fc6d66d4d829fa0
Appolinariya Koval <polinaholod97@gmail.com>
2024-03-05 00:36:11 +0300

    initial hw

---
+++ HomeWorkSeleniumAdvanced/Tests/BaseTest.cs
@@ -0,0 +1,25 @@
+using HomeWorkSeleniumAdvanced.Core;
+using HomeWorkSeleniumAdvanced.Helpers;
+using HomeWorkSeleniumAdvanced.Helpers.Configuration;
+using OpenQA.Selenium;
+
+[Parallelizable(scope: ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+    protected WaitsHelper WaitsHelper { get; private set; }
+
+    [SetUp]
+    public void FactoryDriverTest()
+    {
+        Driver = new Browser().Driver;
+        WaitsHelper = new WaitsHelper(Driver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
