ac30fcefaf1cad82ac1750e290c2a7bc6d2c58b7
Appolinariya Koval <polinaholod97@gmail.com>
2024-03-19 23:51:06 +0300

    second task

--- HomeWorkSeleniumAdvanced/Tests/HomeWorkTests.cs
+++ HomeWorkSeleniumAdvanced/Tests/HomeWorkTests.cs
@@ -12,16 +12,50 @@ public class HomeWorkTests : BaseTest
         //перейти на страницу Context Menu со стартовой страницы
         WaitsHelper.WaitForVisibilityLocatedBy(By.LinkText("Context Menu")).Click();
 
-        //клик правой кнопкой
+        //кликнуть правой кнопкой
         var actions = new Actions(Driver);
         actions
             .ContextClick(Driver.FindElement(By.Id("hot-spot")))
             .Build()
             .Perform();
 
-        //обработка алерта
+        //обработать алерт
         IAlert alert = Driver.SwitchTo().Alert();
         Assert.That(alert.Text, Is.EqualTo("You selected a context menu"));
         alert.Accept();
+    }
+
+    [Test]
+    [Description("Dynamic Controls")]
+    public void DynamicControlsTest()
+    {
+        //перейти на страницу Dynamic Controls со стартовой страницы
+        WaitsHelper.WaitForVisibilityLocatedBy(By.LinkText("Dynamic Controls")).Click();
+
+        //Нажать на кнопку Remove около чекбокса
+        Driver.FindElement(By.XPath("//button[text()='Remove']")).Click();
+
+        //Дождаться надписи “It’s gone” и проверить, что чекбокса нет
+        Assert.Multiple(() =>
+        {
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message")).Text, Is.EqualTo("It's gone!"));
+            Assert.That(WaitsHelper.WaitForElementInvisible(By.Id("checkbox")));
+        });
+
+        //Найти инпут
+        IWebElement input = Driver.FindElement(By.XPath("//input[@type='text']"));
+
+        //Проверить, что инпут disabled
+        Assert.That(!input.Enabled);
+
+        //Нажать на кнопку Enable
+        Driver.FindElement(By.XPath("//button[text()='Enable']")).Click();
+
+        //Дождаться надписи “It's enabled!” и Проверить, что инпут enabled
+        Assert.Multiple(() =>
+        {
+            Assert.That(WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message")).Text, Is.EqualTo("It's enabled!"));
+            Assert.That(input.Enabled);
+        });
     }
 }
