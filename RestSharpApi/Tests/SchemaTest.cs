f810b8a1020ee2532eae3ac58e11b6365a6d4696
Appolinariya Koval <polinaholod97@gmail.com>
2024-03-28 18:06:44 +0300

    50L. Update Schema

--- RestSharpApi/Tests/SchemaTest.cs
+++ RestSharpApi/Tests/SchemaTest.cs
@@ -11,25 +11,26 @@ namespace RestSharpApi.Tests;
 public class SchemaTest
 {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
     private const string BaseRestUri = "https://aqa2504.testrail.io/";
-    
+
     [Test]
     public void JsonSchemaTest()
     {
         const string endpoint = "index.php?/api/v2/add_project";
 
-        Project expectedProject = new Project()
+        Project expectedProject = new Project
         {
-            Name = "Meow WP Project2",
+            Name = "WP Project 2",
             Announcement = "Test project description",
             IsShowAnnouncement = true,
             SuiteMode = 2
         };
-        
-        //Загрузка JSON-схемы из файла
+
+        // Загрузка JSON-схемы из файла
         string schemaJson = File.ReadAllText(@"Resources/schema.json");
-        
-        //Создаем экземпляр JSON-схемы
+
+        // Создем экземпляр JSON-схемы
         JSchema schema = JSchema.Parse(schemaJson);
 
         var options = new RestClientOptions(BaseRestUri)
@@ -37,25 +38,45 @@ public class SchemaTest
             Authenticator = new HttpBasicAuthenticator("atrostyanko@gmail.com", "Qwertyu_1")
         };
 
-        //Setup Rest Client
+        // Setup Rest Client
         var client = new RestClient(options);
 
-        //Setup Request
-        var request = new RestRequest(endpoint)
-            .AddJsonBody(expectedProject);
+        // Setup Request
+        var request = new RestRequest(endpoint).AddJsonBody(expectedProject);
 
-        //Execute Request
+        // Execute Request
         var response = client.ExecutePost(request);
-        
-        //Проверяем статус ответа
+
+        // Проверяем статус ответа
         if (response.StatusCode == HttpStatusCode.OK)
         {
-            //Получаем тело ответа в виде JObject
+            // Получаем тело ответа в виде JObject
             JObject responseData = JObject.Parse(response.Content);
-            
-            //Проверка соответствия ответа JSON-схеме
+
+            // Проверка соответствия ответа JSON-схеме
             Assert.That(responseData.IsValid(schema));
-            //дописать схему, пока тест не работает
         }
+    }
+
+    [Test]
+    public void SchemaTest1()
+    {
+        string schemaJson = @"{
+            'description': 'A person',
+            'type': 'object',
+            'properties':
+                {
+                    'name': {'type':'string'},
+                    'hobbies': {
+                        'type': 'array',
+                        'items': {'type':'string'}
+                    }
+                }
+        }";
+
+        JsonSchema schema = JsonSchema.Parse(schemaJson);
+
+        JObject person = JObject.Parse(@"{'name': 'James','hobbies': ['.NET', 'Blogging', 'Reading', 'Xbox', 'LOLCATS']}");
+        Assert.That(person.IsValid(schema));
     }
 }
