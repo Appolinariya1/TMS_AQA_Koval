9cfe8170ea21418796b1a1faae4147fb19761aef
Appolinariya Koval <polinaholod97@gmail.com>
2024-03-28 18:21:57 +0300

    50L. Bogus (дописать)

--- TestRailComplexApi/Tests/ProjectFakerTest.cs
+++ TestRailComplexApi/Tests/ProjectFakerTest.cs
@@ -1,28 +1,25 @@
 using System.Diagnostics;
+using Bogus;
 using NLog;
+using TestRailComplexApi.Fakers;
 using TestRailComplexApi.Models;
 
 namespace TestRailComplexApi.Tests;
 
-public class ProjectTest : BaseApiTest
+public class ProjectFakerTest : BaseApiTest
 {
     private readonly Logger _logger = LogManager.GetCurrentClassLogger();
     private Project _project = null;
+    private static Faker<Project> Project => new ProjectFaker(); 
 
     [Test]
     [Order(1)]
     public void AddProjectTest()
     {
-        _project = new Project
-        {
-            Name = "Meow",
-            Announcement = "Ks-ks",
-            ShowAnnouncement = true,
-            SuiteMode = 1
-        };
+        _project = Project.Generate();
 
         var actualProject = ProjectService!.AddProject(_project);
-        
+
         //блок проверок
         Assert.Multiple(() =>
         {
@@ -32,6 +29,16 @@ public class ProjectTest : BaseApiTest
             Assert.That(actualProject.Result.SuiteMode, Is.EqualTo(_project.SuiteMode));
         });
         _project = actualProject.Result;
+        _logger.Info(_project.ToString);
+    }
+    
+    
+    [Test]
+    [Order(2)]
+    public void DeleteProjectTest()
+    {
+        Debug.Assert(ProjectService != null, nameof(ProjectService) + " != null");
+        
         _logger.Info(_project.ToString);
     }
 }
