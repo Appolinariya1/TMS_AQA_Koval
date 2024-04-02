2837ac9e0005dca19621ab33de9fcc2fbeefec91
Appolinariya Koval <polinaholod97@gmail.com>
2024-04-02 20:05:56 +0300

    52L. EF_Core

---
+++ DB_EFCore/Connector/DataBaseConnector.cs
@@ -0,0 +1,26 @@
+using DB_EFCore.Helpers.Configuration;
+using DB_EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DB_EFCore.Connector;
+
+public class DataBaseConnector : DbContext
+{
+    public DbSet<Customer>? Customers { get; set; }
+
+    public DataBaseConnector()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = $"Host={Configurator.DbSettings.Server};" +
+                               $"Port={Configurator.DbSettings.Port};" +
+                               $"Database={Configurator.DbSettings.Name};" +
+                               $"User Id={Configurator.DbSettings.Username};" +
+                               $"Password={Configurator.DbSettings.Password};";
+        
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
